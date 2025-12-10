17b18bdff45de4d1b74a1faba3314ea66d04b321
abubakersiddeak <abubaersiddeak@gmail.com>
2025-12-11 01:35:18 +0600

    ubdate cors

--- index.js
+++ index.js
@@ -16,7 +16,12 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://blood-link-bd-ochre.vercel.app"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
